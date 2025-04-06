118ca0872d0a9d61d2e1dda4e7f60f04848dd6ef
Andre Migotto <andremigotto@icloud.com>
2025-04-06 18:10:19 -0300

    Criando rota para export uploads em CSV e acrescentando rota no server.ts

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -10,6 +10,7 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { exportUploadsRoute } from './routes/export-uploads'
 import { getUploadsRoute } from './routes/get-uploads'
 import { uploadImageRoute } from './routes/upload-image'
 import { transformSwaggerSchema } from './transform-swagger-schema'
@@ -57,6 +58,7 @@ server.register(fastifySwaggerUi, {
 
 server.register(uploadImageRoute)
 server.register(getUploadsRoute)
+server.register(exportUploadsRoute)
 
 server
   .listen({
